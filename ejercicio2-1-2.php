2e38f12e9f0c8eb461e2422f2b439c423b66d9c3
DonatoRM <donatoramos@hotmail.com>
2022-10-22 21:08:18 +0200

    Completado el ejercicio 2 con éxito

--- ejercicio2-1-2.php
+++ ejercicio2-1-2.php
@@ -1,29 +1,44 @@
 <!DOCTYPE html>
-<?php function mensaje()
-{
-    static $contador;
-    $contador++;
-    echo "<p>Hola, esta página se ha mostrado $contador veces !</p>";
-} ?>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="application-name" content="UD 2 Características del lenguaje PHP" />
-        <meta name="description" content="Boletín de ejercicios 1" />
-        <meta name="keywords" content="php" />
-        <meta name="author" content="Donato Ramos Martínez" />
-        <title>Título de la página</title>
-        <link rel="stylesheet" type="text/css" href="./css/ejercicio2-1-2.css" media="screen" />
-    </head>   
-    <body>
-        <header>
-            <h1>Ejercicio 2.1.2.</h1>
-        </header>
-        <main>
-            <section>
-                <?php mensaje(); ?>
-            </section>
-        </main>
-    </body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="Content-Type" content="text/html, charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="application-name" content="Ejercicio 2.1.2" />
+    <meta name="description" content="Boletín 2 de la UD2 de Características del lenguaje PHP" />
+    <meta name="keywords" content="html,css,php" />
+    <meta name="author" content="Donato Ramos Martínez" />
+    <title>Ejercicio 2.1.2</title>
+    <link rel="stylesheet" type="text/css" href="./css/ejercicio2-1-2.css" media="screen" />
+  </head>
+  <body>
+    <header>
+      <h1>Ejercicio 2.1.2</h1>
+      <hr />
+    </header>
+    <!-- Código PHP -->
+    <?php
+    session_start();
+    function mostrarNumeroDeVeces()
+    {
+      static $numeroVeces = 0;
+      $_SESSION["mySession"]++;
+      $numeroVeces = $_SESSION["mySession"];
+      echo "<p>Hola, esta página se ha mostrado $numeroVeces veces!</p>";
+    }
+    ?>
+    <!-- Fin de Código PHP -->
+    <main>
+      <section>
+        <!-- Código PHP -->
+        <?php
+        if (!isset($_SESSION["mySession"])) {
+          $_SESSION["mySession"] = 0;
+        }
+        mostrarNumeroDeVeces();
+        ?>
+        <!-- Fin de código PHP -->
+      </section>
+    </main>
+  </body>
 </html>
